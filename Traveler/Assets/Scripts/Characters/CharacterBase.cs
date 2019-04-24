772d97a2451bcbf92af5971a2342ebf81e85f878
DrDoak <larryzhao740@yahoo.com>
2019-04-24 06:48:55 -0500

    Added base character movement controllers

---
+++ Traveler/Assets/Scripts/Characters/CharacterBase.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBase : MonoBehaviour
+{
+    public float GravityScale = 1.0f;
+    public float TimeScale = 1.0f;
+
+    public bool CanControl = true;
+    CharCustomControl m_customControl;
+    CharacterController m_controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_controller = GetComponent<CharacterController>();
+        m_customControl = GetComponent<CharCustomControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CanControl)
+        {
+            Vector3 movement = new Vector3();
+            movement += m_customControl.BaseMovement();
+            movement.y += -GravityScale * Time.deltaTime;
+        }
+
+    }
+}
---
+++ Traveler/Assets/Scripts/Characters/PlayerControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : CharCustomControl
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public override Vector3 BaseMovement()
+    {
+        return new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+    }
+}
